f8d58b6301c367eea715d155609e94a6b51b3831
Sadegh Kazemy <Sadegh.Kazemy@gmail.com>
2015-09-27 16:07:57 +0200

    Add a dummy base activity.

---
+++ library/src/main/java/com/example/library/SimpleBaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.library;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
+
+public abstract class SimpleBaseActivity extends FragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+    }
+}
