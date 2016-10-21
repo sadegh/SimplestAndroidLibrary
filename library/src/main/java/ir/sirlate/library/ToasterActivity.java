91ae880b10b13e766cf7edfc47e4602336d7dd6a
Sadegh Kazemy <Sadegh.Kazemy@gmail.com>
2016-10-21 23:11:52 +0330

    ToasterActivity extends AppCompatActivity.

--- library/src/main/java/ir/sirlate/library/ToasterActivity.java
+++ library/src/main/java/ir/sirlate/library/ToasterActivity.java
@@ -1,11 +1,11 @@
 package ir.sirlate.library;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-public class ToasterActivity extends Activity {
+public class ToasterActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
