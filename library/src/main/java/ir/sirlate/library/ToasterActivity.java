01afb54867c9b5a3e98f83b40a42a6b9ccd19e1e
Sadegh Kazemy <Sadegh.Kazemy@gmail.com>
2016-10-21 23:05:10 +0330

    Rename the activity to ToasterActivity.
     Also added it to Manifest and make it non-abstract.

--- library/src/main/java/ir/sirlate/library/ToasterActivity.java
+++ library/src/main/java/ir/sirlate/library/ToasterActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.widget.Toast;
 
-public abstract class SimpleBaseActivity extends Activity {
+public class ToasterActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
-        Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hi! My name is: " + getClass().getSimpleName(), Toast.LENGTH_LONG).show();
     }
 }
