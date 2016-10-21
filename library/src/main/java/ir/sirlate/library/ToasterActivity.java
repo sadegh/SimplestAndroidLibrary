235f74c069ee0fb5951a3f489aca3e963fc4320d
Sadegh Kazemy <Sadegh.Kazemy@gmail.com>
2016-10-21 23:33:25 +0330

    Fix overriding wrong `onCreate()`

--- library/src/main/java/ir/sirlate/library/ToasterActivity.java
+++ library/src/main/java/ir/sirlate/library/ToasterActivity.java
@@ -1,15 +1,14 @@
 package ir.sirlate.library;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class ToasterActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        Toast.makeText(this, "hi! My name is: " + getClass().getSimpleName(), Toast.LENGTH_LONG).show();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Hi! My name is: " + getClass().getSimpleName(), Toast.LENGTH_LONG).show();
     }
 }
